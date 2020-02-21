182700e52d3d467af81c820f6783ee94bcf7cf96
Sean Wu <seanwu20@gmail.com>
2020-02-21 12:05:02 -0500

    fixed some bugs (#5)

--- canada_adv/serializers.py
+++ canada_adv/serializers.py
@@ -5,7 +5,7 @@ from .models import UserInfo
 class UserInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserInfo
-        fields = ['user_id', 'food', 'water',
+        fields = ['user_id', 'user_food', 'user_water',
                   'state', 'city',
                   'location', 'food_available', 'water_available',
                   'location_2', 'food_available_2', 'water_available_2']
