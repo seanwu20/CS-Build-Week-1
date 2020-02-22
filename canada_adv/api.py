02abb42bde2b79533dde0b6e5b0395f6c41f28c2
Sean Wu <seanwu20@gmail.com>
2020-02-22 12:30:18 -0500

    simplify endpoints, added auth

--- canada_adv/api.py
+++ canada_adv/api.py
@@ -4,79 +4,78 @@ from .utils import Map, random_generator_pick_2
 
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from .serializers import UserInfoSerializer
 from django.core.exceptions import ObjectDoesNotExist
 
+from rest_framework.permissions import IsAuthenticated
 import random
 
 
 class UserInfoViewSet(viewsets.ModelViewSet):
     queryset = UserInfo.objects.all()
     serializer_class = UserInfoSerializer
+    permission_classes = [IsAuthenticated]
 
     def retrieve(self, request, *args, **kwargs):
         us_map = Map()
         us_map.populate_map()
         try:
-            player_data = UserInfo.objects.values().get(user_id = self.kwargs['pk'])
-            current_city = us_map.search_map(player_data.get('city'))
-            print(current_city)
+            user_data = UserInfo.objects.values().get(user_id=self.kwargs['pk'])
+            current_city = us_map.search_map(user_data.get('city'))
             if current_city == -1:
                 return Response("Player City does not exit")
             elif current_city != -1:
-                player_data['left'] = current_city.left.city if current_city.left else None
-                player_data['right'] = current_city.right.city if current_city.right else None
-                player_data['previous'] = current_city.previous.city if current_city.previous else None
-            return Response(player_data)
+                user_data['left'] = current_city.left.city if current_city.left else None
+                user_data['right'] = current_city.right.city if current_city.right else None
+                user_data['previous'] = current_city.previous.city if current_city.previous else None
+            return Response(user_data)
         except ObjectDoesNotExist:
             return Response("Invalid user_id")
 
+    def update(self, request, *args, **kwargs):
+        # user id, next_city user chooses, food, water
+        random_places = random_generator_pick_2()
 
+        us_map = Map()
+        us_map.populate_map()
+        try:
+            new_city = us_map.search_map(request.data.get('new_city'))
 
-@api_view(["PUT"])
-def move_city(request):
-    # user id, next_city user chooses, food, water
-    random_places = random_generator_pick_2()
+            user_data = UserInfo.objects.get(user_id=self.kwargs['pk'])
 
-    us_map = Map()
-    us_map.populate_map()
-    try:
-        new_city = us_map.search_map(request.data.get('new_city'))
+            user_data.city = new_city.city
 
-        left = new_city.left.city if new_city.left else None
-        right = new_city.right.city if new_city.right else None
-        previous = new_city.previous.city if new_city.previous else None
+            user_data.user_food = request.data.get('user_food')
+            user_data.user_water = request.data.get('user_water')
 
-        player = UserInfo.objects.get(user_id=request.data.get('user_id'))
-        player.city = request.data.get('new_city')
-        player.user_food = request.data.get('user_food')
-        player.user_water = request.data.get('user_water')
+            user_data.location = random_places[0]
+            user_data.food_available = random.randint(1, 10)
+            user_data.water_available = random.randint(1, 10)
+            user_data.state = new_city.state
 
-        player.location = random_places[0]
-        player.food_available = random.randint(1, 10)
-        player.water_available = random.randint(1, 10)
-        player.state = new_city.state
+            user_data.location_2 = random_places[1]
+            user_data.food_available_2 = random.randint(1, 10)
+            user_data.water_available_2 = random.randint(1, 10)
 
-        player.location_2 = random_places[1]
-        player.food_available_2 = random.randint(1, 10)
-        player.water_available_2 = random.randint(1, 10)
+            user_data.save()
 
-        player.save()
+            player_data = UserInfo.objects.values().get(user_id=self.kwargs['pk'])
 
-        player_data = UserInfo.objects.values().get(user_id=request.data.get('user_id'))
+            left = new_city.left.city if new_city.left else None
+            right = new_city.right.city if new_city.right else None
+            previous = new_city.previous.city if new_city.previous else None
+            player_data['left'] = left
+            player_data['right'] = right
+            player_data['previous'] = previous
+            return Response(player_data)
 
-        player_data['left'] = left
-        player_data['right'] = right
-        player_data['previous'] = previous
-
-        return Response(player_data)
-
-    except ObjectDoesNotExist:
-        return Response("Invalid User Id")
+        except ObjectDoesNotExist:
+            return Response("Invalid User Id")
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def map_endpoint(request):
     us_map = Map()
     us_map.populate_map()
--- canada_adv/urls.py
+++ canada_adv/urls.py
@@ -14,16 +14,14 @@ router = routers.DefaultRouter()
 router.register('userinfo', api.UserInfoViewSet)
 
 urlpatterns = [
-    path(r'', include(router.urls)),
-
-    path("", api.UserInfoViewSet),
-    path("move/", api.move_city),
-    path("map/", api.map_endpoint),
-
-
     url('register/', include('rest_auth.registration.urls')),
-    path('', include('rest_auth.urls')),
+    url('', include('rest_auth.urls')),
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path(r'', include(router.urls)),
+
+    path("map/", api.map_endpoint),
 
 ]
