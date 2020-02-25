aec70191ff68db696c855776f939b02cc4ae1487
Sean Wu <seanwu20@gmail.com>
2020-02-25 04:02:40 -0500

    More cleanup (#6)
    * fixed some bugs
    
    * updated modelviewset
    
    * simplify endpoints, added auth
    
    * changes

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -112,18 +113,13 @@ AUTH_PASSWORD_VALIDATORS = [
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-
     ),
 }
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7)
+}
 
 REST_USE_JWT = True
 
--- canada_adv/models.py
+++ canada_adv/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
-
 class UserInfo(models.Model):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     user_food = models.IntegerField()
@@ -17,6 +14,3 @@ class UserInfo(models.Model):
     location_2 = models.CharField(max_length=30, blank=False)
     food_available_2 = models.IntegerField(blank=False)
     water_available_2 = models.IntegerField(blank=False)
-
-    def __str__(self):
-        return UserInfo.user_id
